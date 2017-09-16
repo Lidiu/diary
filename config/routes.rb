4d2333ec73f866d74e6a772b3b300c1f86b8c1b2
Lidka Szpara <lidiaszpara@o2.pl>
2017-09-16 17:23:20 +0200

    update roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index', to: 'welcome#index'
-  get '/', to: 'welcome#index'
+  get '/', to: 'entries#index'
   get 'tralala' ,  to: 'welcome#index'
-  resources "entries"
+  get 'entries/', to: 'entries#index'
+  get 'entries/:id', to: 'entries#show', as: "entry"
   # root 'welcome#index'
   #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
