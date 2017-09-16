2526c58ef043f9e3331c7688f5a734d05f045bb6
Lidka Szpara <lidiaszpara@o2.pl>
2017-09-16 18:00:02 +0200

    update2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/', to: 'entries#index'
   get 'tralala' ,  to: 'welcome#index'
   get 'entries/', to: 'entries#index'
+  get 'entries/new', to: 'entries#new', as: "new_entry"
   get 'entries/:id', to: 'entries#show', as: "entry"
   # root 'welcome#index'
   #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
